57e96a435f50fd91d217fae6477cf3937c439933
Cabomaldade <soutom@yahoo.com.br>
2019-05-19 19:54:54 -0300

    Oba, funcionou aqui, agora só subir isso e o servidor

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -5,6 +5,7 @@ import { Http,Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ClientesService {
